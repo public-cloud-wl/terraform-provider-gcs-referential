5a790b2bc4a1bc80571eb4d947e8f1cf6cc56019
Maarc-D <escasgorille@aol.com>
2024-02-14 09:55:42 +0100

    feat(generalization): add retry on read trying to avoir error 412 form goole could due to lock

--- internal/provider/resource_network_request.go
+++ internal/provider/resource_network_request.go
@@ -10,6 +10,7 @@ import (
 	"github.com/public-cloud-wl/terraform-provider-gcsreferential/internal/provider/connector"
 	"strconv"
 	"strings"
+  "time"
 )
 
 func networkRequest() *schema.Resource {
@@ -99,6 +100,9 @@ func retry(toRetry func() error) error {
 			break
 		}
 		n++
+    fmt.Printf("Issue on request retry: %d, -> try again", n)
+    sleepDuration, _ := time.ParseDuration(fmt.Sprintf("%ds", 2*n))
+    time.Sleep(sleepDuration)
 	}
 	return err
 }
@@ -138,6 +142,7 @@ func innerResourceServerCreate(ctx context.Context, data *schema.ResourceData, m
 			return err
 		}
 		networkConfig.Subnets[netmaskId] = nextNetmask
+    //err = gcpConnector.RecursiveRetryReadWrite
 		err = gcpConnector.WriteRemote(networkConfig, ctx)
 		if err != nil {
 			return err
